5a4a35aea4fa9d5728d4e5e326c1b0cab6453018
Elpala <luis.pala1997@gmail.com>
2019-06-07 11:50:03 -0500

    finished user/psw

--- getty.c
+++ getty.c
@@ -8,42 +8,58 @@ int main(int argc, char const *argv[]) {
   char user[100];
   char psw[100];
   char aux[100];
-  char aux2[100];
+  char *aux2;
   FILE * passwd;
   int bool = 1;
   int len=0;
   const char str[] = ":";
-  printf("%s\n", str);
-  passwd = fopen("passwd.txt", "r");
-  if(passwd==NULL){
-    return 0;
-  }
+
   while (1) {
       do {
-    		printf("User:");
-    		scanf("%[^\n]%*c",user);
-        while(fgets(aux,100,passwd))
-        {
-          printf("%s\n", aux);
-          len = strcspn(aux, str);
-          strcpy(aux2,"");
-          printf("%s\n", aux2);
-          strncpy ( aux2, aux, len);
-          printf("%s\n", aux2);
-          if(strcmp(user, aux2)==0){
-              bool=0;
-              printf("holaaa\n", aux2);
-              break;
+      		printf("User:");
+          fflush(stdin);
+
+      		scanf("%[^\n]%*c",user);
+          passwd = fopen("passwd.txt", "r");
+          if(passwd==NULL){
+            return 0;
+          }else
+
+          strcpy(aux,"");
+
+
+          while(fgets(aux,100,passwd))
+          {
+
+
+            len = strcspn(aux, str);
+            aux2=memchr(aux,':',len+1);
+            *(aux2++);
+            printf("%d\n", strlen(aux));
+            aux[strlen(aux)-1]='\0';
+            //strcpy(aux2,"");
+            //strncpy ( aux, aux, len);
+            aux[len]='\0';
+
+            if(strcmp(user, aux)==0){
+                bool=0;
+                printf("Usuario correcto, hola %s\n",aux);
+                break;
+            }
           }
-        }
+          fclose(passwd);
     	} while(bool);
-      printf("user correct\n");
-      do {
-    		printf("psw:");
-    		scanf("%[^\n]%*c",psw);
-        //buscar en el archivo
-    	} while(strcmp(psw,"nena1")!=0);
-      printf("password correct\n");
+      bool=1;
+  		printf("psw:");
+      fflush(stdin);
+      scanf("%[^\n]%*c",psw);
+      printf("%s\n", aux2);
+      printf("%s\n", psw);
+    	if(strcmp(psw,aux2)==0){
+        printf("password correcta\n");
+      }else
+        printf("password incorrecta\n");
+
   }
 
   return 0;
