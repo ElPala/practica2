e94747c6321329acf3d8f38bef06fc09252e7c50
yo <alexmora95>
2019-06-07 12:42:16 -0500

    shutdown

--- sh.c
+++ sh.c
@@ -1,20 +1,3 @@
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-int main(int argc, char const *argv[]) {
-  char cmd[80];
-
-	do {
-		printf("Shell >");
-		scanf("%[^\n]%*c",cmd);
-		system(cmd);
-	} while(strcmp(cmd,"exit")!=0);
-	return 0;
-}
-*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -25,7 +8,6 @@ int main()
 {
     char cmd[80];
     char entrada[100];
-    int bandera1 = 0;
 
     while(1){
       printf("Shell >");
@@ -34,31 +16,28 @@ int main()
 
 
       if (strcmp(entrada,"exit")==0){
-        printf("ola\n" );
-        bandera1 = 1;
-        break;
+        execlp("./getty","getty",NULL );
       }
 
-    //  else   if (strcmp(entrada,"./exec.c")==0){
-      //    execlp("EXEC.c",NULL);
-        //}
 
-
-      else if (entrada == "shutdown"){
-        //kill
+      if (strcmp(entrada,"shutdown")==0){
+        //kill all
+        sleep(5);
+        printf("pala es gei");
+        execlp ("killall", "killall", "-9", "xterm","init", NULL);
       }
 
       pid_t p;
       p = fork();
-      if(p==0)
-          execlp(entrada, entrada, NULL);
+      if(p==0){
+          printf("pala es gei");
+                  execlp(entrada, entrada, NULL);
+
+      }
     }//end while
 
 
-      if (bandera1 = 1) {
-        execlp("./getty","getty",NULL );
 
-      }
       printf("Ending-----");
 
     return 0;
